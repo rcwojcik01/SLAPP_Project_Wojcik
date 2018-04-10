ac12e0528b3709965ffc949acf792e4c6ae79ec8
GameFishDevelopment <gamefishdevelopment@gmail.com>
2018-04-09 21:55:52 -0500

    client side new look

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     var socket = io.connect('http://10.10.90.78:3000'); // create connection
     socket.on('valuesReceived', function(data) {
-        $('#air-quality').text("Air Quality Value: " + data.airQuality);
+        $('#air-quality').text(data.airQuality);
+//        $('#orientation').text(data.orientation);
+//        $('#vibration').text(data.vibration);
+//        $('#accelerometer').text(data.accelerometer);
     });
 });
