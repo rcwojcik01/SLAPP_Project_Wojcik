8ef2b63f917436d4e7b2fa32a0f0754d50f8b919
Ross Wojcik <rosswojcik@RossMacbookPro.local>
2018-04-11 11:34:42 -0500

    Fixed screw up

---
+++ Python/Python.py
@@ -0,0 +1,37 @@
+-import time
+-import requests
+-import grovepi
+-import sys
+-
+-# A0
+-# SIG,NC,VCC,GND
+-air_sensor = 0
+-ultrasonic_ranger = 4
+-
+-url = "http://RossMacbookPro.local:3000"
+-
+-while True:
+-    try:
+-        sensor_value = grovepi.analogRead(air_sensor)
+-        if sensor_value <= 291:
+-            air_quality = 'good'
+-        else:
+-            air_quality = 'bad'
+-
+-        print(sensor_value)
+-        print(air_quality)
+-
+-        dist_value = grovepi.ultrasonicRead(ultrasonic_ranger)
+-
+-        print(dist_value)
+-
+-        payload = { 'airQuality' : air_quality, 'airValue' : sensor_value, 'distance' : dist_value}
+-
+-        requests.post(url, data=payload)
+-
+-        time.sleep(.5)
+-
+-    except TypeError:
+-        print ("Error")
+-    except IOError:
+-        print ("Error")
