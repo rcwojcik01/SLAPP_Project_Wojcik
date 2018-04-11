62bb65f1b221da5b57b4d0a5cda7b0047d5087bc
Ross Wojcik <rosswojcik@RossMacbookPro.local>
2018-04-11 15:38:56 -0500

    Python update 10: temp and light sensors added

--- Python/Python.py
+++ Python/Python.py
@@ -8,11 +8,13 @@ import sys
 air_sensor = 0
 ultrasonic_ranger = 4
 temp_sensor = 1
+light_sensor = 2
 
 url = "http://RossMacbookPro.local:3000"
 
 while True:
     try:
+        # Air Sensor
         sensor_value = grovepi.analogRead(air_sensor)
         if sensor_value <= 291:
             air_quality = 'good'
@@ -21,20 +23,31 @@ while True:
 
         print(sensor_value)
         print(air_quality)
-
+        
+        # Distance Sensor
         dist_value = grovepi.ultrasonicRead(ultrasonic_ranger)
 
         print(dist_value)
         
-        temp = grovepi.temp(temp_sensor, '1.1')
-        print("temp =", temp)
-
-        payload = { 'airQuality' : air_quality, 'airValue' : sensor_value, 'distance' : dist_value}
+        # Temperature and Humidity Sensor
+        
+        temp_value = grovepi.temp(temp_sensor, '1.1')
+        print("temp =", temp_value)
+        
+        # Light Sensor
+        
+        light_value = grovepi.analogRead(light_sensor)
+        print("light_value = %d" %(light_value))
+        
+        # Broadcast Payload
+        payload = { 'airQuality' : air_quality, 'airValue' : sensor_value, 'distance' : dist_value, 'tempValue' : temp_value, 'lightValue' : light_value}
 
         requests.post(url, data=payload)
 
         time.sleep(.5)
 
+    # Errors
+    
     except TypeError:
         print ("Error")
     except IOError:
