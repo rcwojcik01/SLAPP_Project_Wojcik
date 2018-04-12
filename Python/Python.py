3de907e0bbc06b83b435547766127c2afffb8d83
Ross Wojcik <rcwojcik01@gmail.com>
2018-04-12 11:41:35 -0500

    Python update 12

--- Python/Python.py
+++ Python/Python.py
@@ -21,32 +21,31 @@ while True:
         else:
             air_quality = 'bad'
 
-            print(sensor_value)
-            print(air_quality)
+        print('Air Sensor Value',sensor_value)
+        print('Air Quality',air_quality)
         
         # Distance Sensor
         dist_value = grovepi.ultrasonicRead(ultrasonic_ranger)
 
-            print(dist_value)
-"""
-        
+        print('Distance',dist_value)
+
         # Temperature and Humidity Sensor
         
-        temp_value = grovepi.temp(temp_sensor, '1.1')
+        temp_value = grovepi.temp(temp_sensor, model= '1.2')
         print("temp =", temp_value)
         
         # Light Sensor
         
-        light_value = grovepi.analogRead(light_sensor)
-        print("light_value = %d" %(light_value))
+        #light_value = grovepi.analogRead(light_sensor)
+        #print("light_value = %d" %(light_value))
+        #""", 'tempValue' : temp_value, 'lightValue' : light_value"""
         
-"""
         # Broadcast Payload
-        payload = { 'airQuality' : air_quality, 'airValue' : sensor_value, 'distance' : dist_value""", 'tempValue' : temp_value, 'lightValue' : light_value"""}
+        payload = { 'airQuality' : air_quality, 'airValue' : sensor_value, 'distance' : dist_value}
 
         requests.post(url, data=payload)
 
-        time.sleep(.5)
+        time.sleep(1)
 
     # Errors
     
